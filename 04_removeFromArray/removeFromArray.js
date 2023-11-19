b3d7b034c7866c996d53d37b37f016a0c78dec18
Gregoire Schneeberger <60151429+gregoire-schneeberger@users.noreply.github.com>
2023-11-19 12:23:26 +0100

    fin ex js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToDelet) {
+    const args = Array.from(elementsToDelet);
 
+    function remove(array, element) {
+        let index = array.indexOf(element);
+        array.splice(index,1);
+    }
+    
+    for (let i = 0; i<args.length; i++) {
+        if (array.includes(args[i])) {
+            remove(array, args[i])};
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let sum = 0 ; 
 
+    if (typeof(firstInt) !== "number" || typeof(secondInt) !== "number" || firstInt < 0 ||  secondInt < 0) {
+        return "ERROR";
+    }
+
+    if (firstInt < secondInt) {
+        for (i = firstInt; i< secondInt +1; i++) {
+            sum += i;
+        }
+    }
+    else if (firstInt > secondInt) {
+        for (i = secondInt; i< firstInt +1; i++) 
+        {
+        sum += i;
+        }
+    }
+    else {
+        sum = firstInt;
+    }
+    return sum;
 };
 
 // Do not edit below this line
